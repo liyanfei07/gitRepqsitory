7ca5ed790115c0a20471d548a7701bc493dbcaed
tarena <tarena@liyanfei.local>
2015-08-25 19:24:22 +0800

    qswqdq

---
+++ 的速度是多少/的速度是多少/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  的速度是多少
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 的速度是多少/的速度是多少/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  的速度是多少
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"24543");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
